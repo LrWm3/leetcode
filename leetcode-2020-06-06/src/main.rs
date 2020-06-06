e7d999e702a1baf69547cebc187e6ac52d97efb2
William Marsman <william.marsman@gmail.com>
2020-06-06 16:30:09 -0300

    passed the test lmao this is rough.

--- leetcode-2020-06-06/src/main.rs
+++ leetcode-2020-06-06/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp;
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
@@ -31,7 +33,8 @@ impl Solution {
       let mut peoples_height_inserted_desc: Vec<i32> = vec![std::i32::MAX];
       peoples_height_inserted_desc.reserve(people_len + 1);
 
-      let mut people_reconstruct: Vec<Vec<i32>> = vec![vec![]; people_len];
+      let mut people_reconstruct: Vec<Vec<i32>> = vec![];
+      people_reconstruct.reserve(people_len);
 
       // We want to have room for the 0 -> most_people_in_front + 1.
       let people_stack_size: usize = (people_copy.iter()
@@ -60,18 +63,19 @@ impl Solution {
       println!("peoples_height_inserted_desc: {:?}", peoples_height_inserted_desc);
       println!("people_reconstruct:: {:?}", people_reconstruct);
 
-
       for person_idx in 0..people_len {
         println!("finding person 1... {}", person_idx);
-        let peoples_height_inserted_len = peoples_height_inserted_desc.len() - 1;
-        
+        let peoples_height_inserted_len = cmp::min(
+          peoples_height_inserted_desc.len(), 
+          people_stacks.len());
+
         // Start with the tallest and work our way backwards
         for target_height in (0..peoples_height_inserted_len).rev() {
           println!("For person {}, trying height {}", person_idx, target_height);
           let person_stack = &mut people_stacks[target_height];
           if person_stack.as_ref().and_then(|ps| ps.last()).map_or(false, |person_small| {
             let person_height = person_small[height_idx];
-            if person_height < peoples_height_inserted_desc[target_height] {
+            if person_height <= peoples_height_inserted_desc[target_height] {
                 
                 peoples_height_inserted_desc.insert(target_height + 1, person_height);
                 people_reconstruct.push(person_small.to_vec());
