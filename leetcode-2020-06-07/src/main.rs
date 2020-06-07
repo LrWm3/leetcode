886bef8391bd11fb6cc3126ec0eefa3e072d4124
William Marsman <william.marsman@gmail.com>
2020-06-07 20:14:37 -0300

    Add command-line example.

--- leetcode-2020-06-07/src/main.rs
+++ leetcode-2020-06-07/src/main.rs
@@ -5,13 +5,18 @@ use std::env;
 // Usage
 //   see help()
 //
-fn help() {
-  println!(
+fn help(args: Vec<String>) {
+    println!(
     "
   2020-06-07
+  url: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3353/
+
+  Problem
+
   You are given coins of different denominations and a total amount of money.
   Write a function to compute the number of combinations that make up that amount.
   You may assume that you have infinite number of each kind of coin.
+
   > Example 1:
   > 
   > Input: amount = 5, coins = [1, 2, 5]
@@ -21,7 +26,7 @@ fn help() {
   > 5=2+2+1
   > 5=2+1+1+1
   > 5=1+1+1+1+1
-  > 
+
   > Example 2:
   > 
   > Input: amount = 3, coins = [2]
@@ -31,121 +36,128 @@ fn help() {
   > 
   > Input: amount = 10, coins = [10] 
   > Output: 1
+
   Note:
-  You can assume that
-  0 <= amount <= 5000
-  1 <= coin <= 5000
-  the number of coins is less than 500
-  the answer is guaranteed to fit into signed 32-bit integer
+
+  You can assume that:
+
+  • 0 <= amount <= 5000
+  • 1 <= coin <= 5000
+  • the number of coins is less than 500
+  • the answer is guaranteed to fit into signed 32-bit integer
+  
   Usage
-  <program_name> [amount] (coin1 coin2 coin 3 .. coin N)"
+    {} [amount] (coin1 coin2 coin3 .. coinN)
+  ", args[0]
   );
-}
-
-#[cfg(test)]
-mod tests {
-  // Note this useful idiom: importing names from outer (for mod tests) scope.
-  use super::*;
-
-  #[test]
-  fn test_change_1() {
-    let coins = vec![1, 2, 5];
-    let amount = 5;
-    assert_eq!(4, Solution::change(amount, coins));
-  }
-
-  #[test]
-  fn test_change_2() {
-    let coins = vec![2, 3, 5];
-    let amount = 5;
-    assert_eq!(2, Solution::change(amount, coins));
-  }
-
-  #[test]
-  fn test_change_w_zero_amount() {
-    let coins = vec![1, 2, 3];
-    let amount = 0;
-    assert_eq!(1, Solution::change(amount, coins));
-  }
-
-  #[test]
-  fn test_change_w_zero_coins() {
-    let coins: Vec<i32> = vec![];
-    let amount = 1;
-    assert_eq!(0, Solution::change(amount, coins));
-  }
-
-  #[test]
-  fn test_change_w_amount_as_one() {
-    let coins: Vec<i32> = vec![2, 3];
-    let amount = 1;
-    assert_eq!(0, Solution::change(amount, coins));
-  }
-
-  #[test]
-  fn test_change_w_amount_as_one_and_coin_as_one() {
-    let coins: Vec<i32> = vec![1, 2, 3];
-    let amount = 1;
-    assert_eq!(1, Solution::change(amount, coins));
-  }
 }
 
 struct Solution {}
 
 impl Solution {
-  pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
-    let extent = amount + 1;
-    let mut num_ways_for_amt: Vec<i32> = vec![0; extent as usize];
+    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
+        let extent = amount + 1;
+        let mut num_ways_for_amt: Vec<i32> = vec![0; extent as usize];
 
-    // when coin - amt_idx = 0, add 1
-    num_ways_for_amt[0] = 1;
+        // when coin - amt_idx = 0, add 1
+        num_ways_for_amt[0] = 1;
 
-    // process bottom up to use previous num ways to compute latter
-    for coin in coins {
-      for amt_idx in coin..extent {
-        num_ways_for_amt[amt_idx as usize] += num_ways_for_amt[(amt_idx - coin) as usize];
-      }
+        // process bottom up to use previous num ways to compute latter
+        for coin in coins {
+            for amt_idx in coin..extent {
+                num_ways_for_amt[amt_idx as usize] += num_ways_for_amt[(amt_idx - coin) as usize];
+            }
+        }
+        return num_ways_for_amt[amount as usize];
     }
-    return num_ways_for_amt[amount as usize];
-  }
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_change_1() {
+        let coins = vec![1, 2, 5];
+        let amount = 5;
+        assert_eq!(4, Solution::change(amount, coins));
+    }
+
+    #[test]
+    fn test_change_2() {
+        let coins = vec![2, 3, 5];
+        let amount = 5;
+        assert_eq!(2, Solution::change(amount, coins));
+    }
+
+    #[test]
+    fn test_change_w_zero_amount() {
+        let coins = vec![1, 2, 3];
+        let amount = 0;
+        assert_eq!(1, Solution::change(amount, coins));
+    }
+
+    #[test]
+    fn test_change_w_zero_coins() {
+        let coins: Vec<i32> = vec![];
+        let amount = 1;
+        assert_eq!(0, Solution::change(amount, coins));
+    }
+
+    #[test]
+    fn test_change_w_amount_as_one() {
+        let coins: Vec<i32> = vec![2, 3];
+        let amount = 1;
+        assert_eq!(0, Solution::change(amount, coins));
+    }
+
+    #[test]
+    fn test_change_w_amount_as_one_and_coin_as_one() {
+        let coins: Vec<i32> = vec![1, 2, 3];
+        let amount = 1;
+        assert_eq!(1, Solution::change(amount, coins));
+    }
 }
 
 fn main() -> Result<(), ()> {
-  let args: Vec<String> = env::args().collect();
-  match args.len() {
-    // no arguments passed
-    1 => {
-      help();
-      println!("No arguments provided!");
-      return Result::Err(());
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        // no arguments passed
+        1 => {
+            help(args);
+            println!("No arguments provided!");
+            return Result::Err(());
+        }
+        // one argument passed
+        2 => {
+            let amount = args[1].parse::<i32>().expect("Failed to parse 'amount'");
+            let coins = vec![];
+            println!(
+                "amount={}, coins={:?}, coin_permutations={}",
+                amount,
+                coins,
+                Solution::change(amount, coins.to_vec())
+            );
+            return Result::Ok(());
+        }
+        // all the other cases
+        _ => {
+            let amount = args[1].parse::<i32>().expect("Failed to parse 'amount'");
+            let coins: Vec<i32> = args[2..]
+                .iter()
+                .map(|coin| {
+                    coin.parse::<i32>()
+                        .expect("Failed to parse one of the provided 'coin'")
+                })
+                .collect();
+            println!(
+                "amount={}, coins={:?}, coin_permutations={}",
+                amount,
+                coins,
+                Solution::change(amount, coins.to_vec())
+            );
+            return Result::Ok(());
+        }
     }
-    // one argument passed
-    2 => {
-      let amount = args[0]
-        .parse::<i32>()
-        .expect("Failed to parse amount from first argument, did you provide a number?");
-      let coins = vec![];
-      println!(
-        "amount={}, coins={:?}, coin_permutations={}",
-        amount,
-        coins,
-        Solution::change(amount, coins.to_vec())
-      );
-      return Result::Ok(());
-    }
-    // all the other cases
-    _ => {
-      let amount = args[0]
-        .parse::<i32>()
-        .expect("Failed to parse amount from first argument, did you provide a number?");
-      let coins = vec![];
-      println!(
-        "amount={}, coins={:?}, coin_permutations={}",
-        amount,
-        coins,
-        Solution::change(amount, coins.to_vec())
-      );
-      return Result::Ok(());
-    }
-  }
 }
