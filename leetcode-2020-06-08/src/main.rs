e2d73cc4702bd50f0d1e8a8d9c734952554e9167
Will Marsman <william.marsman@mdacorporation.com>
2020-06-08 10:25:49 -0300

    leetcode-2020-06-08: 2020-06-08 solution

--- leetcode-2020-06-08/src/main.rs
+++ leetcode-2020-06-08/src/main.rs
@@ -6,8 +6,8 @@ use std::env;
 //   see help()
 //
 fn help(args: Vec<String>) {
-    println!(
-        "
+  println!(
+    "
     2020-06-08
     url: [add URL]
 
@@ -25,68 +25,85 @@ fn help(args: Vec<String>) {
         [add usage]
         {} [amount] (coin1 coin2 coin3 .. coinN)
 ",
-        args[0]
-    );
+    args[0]
+  );
 }
 
 struct Solution {}
 
 impl Solution {
-    // TODO - add interface
-    pub fn change() -> i32 {
-        return 0;
-    }
+  pub fn is_power_of_two(n: i32) -> bool {
+    const POWS: [i32; 31] = [
+      0x00000001, 0x00010000, 0x00000002, 0x00020000, 0x00000004, 0x00040000, 0x00000008,
+      0x00080000, 0x00000010, 0x00100000, 0x00000020, 0x00200000, 0x00000040, 0x00400000,
+      0x00000080, 0x00800000, 0x00000100, 0x01000000, 0x00000200, 0x02000000, 0x00000400,
+      0x04000000, 0x00000800, 0x08000000, 0x00001000, 0x10000000, 0x00002000, 0x20000000,
+      0x00004000, 0x40000000, 0x00008000,
+    ];
+    return POWS.contains(&n);
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::Solution;
+  // Note this useful idiom: importing names from outer (for mod tests) scope.
+  use super::Solution;
 
-    #[test]
-    fn test_1() {
-        assert_eq!(0, Solution::change());
-    }
+  #[test]
+  fn test_0() {
+    let amount = 0;
+    assert_eq!(true, Solution::is_power_of_two(amount));
+  }
+
+  #[test]
+  fn test_1() {
+    let amount = 1;
+    assert_eq!(true, Solution::is_power_of_two(amount));
+  }
+
+  #[test]
+  fn test_2() {
+    let amount = 2;
+    assert_eq!(true, Solution::is_power_of_two(amount));
+  }
+
+  #[test]
+  fn test_3() {
+    let amount = 3;
+    assert_eq!(false, Solution::is_power_of_two(amount));
+  }
+
+  #[test]
+  fn test_4() {
+    let amount = (2 as i32).pow(30);
+    assert_eq!(false, Solution::is_power_of_two(amount));
+  }
 }
 
 fn main() -> Result<(), ()> {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        // no arguments passed
-        1 => {
-            help(args);
-            println!("No arguments provided!");
-            return Result::Err(());
-        }
-        // one argument passed
-        2 => {
-            let amount = args[1].parse::<i32>().expect("Failed to parse 'amount'");
-            let coins = vec![];
-            println!(
-                "amount={}, coins={:?}, coin_permutations={}",
-                amount,
-                coins,
-                Solution::change()
-            );
-            return Result::Ok(());
-        }
-        // all the other cases
-        _ => {
-            let amount = args[1].parse::<i32>().expect("Failed to parse 'amount'");
-            let coins: Vec<i32> = args[2..]
-                .iter()
-                .map(|coin| {
-                    coin.parse::<i32>()
-                        .expect("Failed to parse one of the provided 'coin'")
-                })
-                .collect();
-            println!(
-                "amount={}, coins={:?}, coin_permutations={}",
-                amount,
-                coins,
-                Solution::change()
-            );
-            return Result::Ok(());
-        }
+  let args: Vec<String> = env::args().collect();
+  match args.len() {
+    // no arguments passed
+    1 => {
+      help(args);
+      println!("No arguments provided!");
+      return Result::Err(());
     }
+    // one argument passed
+    2 => {
+      let amount = args[1].parse::<i32>().expect("Failed to parse 'amount'");
+      println!(
+        "amount={}, is_power_of_two={}",
+        amount,
+        Solution::is_power_of_two(amount)
+      );
+      return Result::Ok(());
+    }
+    // all the other cases
+    _ => {
+      help(args);
+      println!("Too many arguments provided!");
+      return Result::Ok(());
+    }
+  }
 }
